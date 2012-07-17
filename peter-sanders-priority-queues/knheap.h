e42dc37f6be271a79361e01b73f3014f37aef46c
Shlomi Fish <shlomif@shlomifish.org>
2012-07-17 21:15:41 +0300

    Remove trailing space.

--- peter-sanders-priority-queues/knheap.h
+++ peter-sanders-priority-queues/knheap.h
@@ -3,13 +3,13 @@
 #define KNHEAP
 #include "util.h"
 
-const int KNBufferSize1 = 32; // equalize procedure call overheads etc. 
+const int KNBufferSize1 = 32; // equalize procedure call overheads etc.
 const int KNN = 512; // bandwidth
 const int KNKMAX = 64;  // maximal arity
 const int KNLevels = 4; // overall capacity >= KNN*KNKMAX^KNLevels
 const int LogKNKMAX = 6;  // ceil(log KNK)
 /*
-const int KNBufferSize1 = 3; // equalize procedure call overheads etc. 
+const int KNBufferSize1 = 3; // equalize procedure call overheads etc.
 const int KNN = 10; // bandwidth
 const int KNKMAX = 4;  // maximal arity
 const int KNLevels = 4; // overall capacity >= KNN*KNKMAX^KNLevels
@@ -28,7 +28,7 @@ class BinaryHeap {
   Element data[capacity + 2];
   int size;  // index of last used element
 public:
-  BinaryHeap(Key sup, Key infimum):size(0) { 
+  BinaryHeap(Key sup, Key infimum):size(0) {
     data[0].key = infimum; // sentinel
     data[capacity + 1].key = sup;
     reset();
@@ -60,7 +60,7 @@ reset() {
     data[i].key = sup;
   }
   // if this becomes a bottle neck
-  // we might want to replace this by log KNN 
+  // we might want to replace this by log KNN
   // memcpy-s
 }
 
@@ -71,7 +71,7 @@ deleteMin()
   Assert2(size > 0);
 
   // first move up elements on a min-path
-  int hole = 1; 
+  int hole = 1;
   int succ = 2;
   int sz   = size;
   while (succ < sz) {
@@ -155,7 +155,7 @@ insert(Key k, Value v)
   Debug4(cout << "insert(" << k << ", " << v << ")" << endl);
 
   size++;
-  int hole = size; 
+  int hole = size;
   int pred = hole >> 1;
   Key predKey = data[pred].key;
   while (predKey > k) { // must terminate due to sentinel at 0
@@ -196,7 +196,7 @@ class KNLooserTree {
 
   // upper levels of looser trees
   // entry[0] contains the winner info
-  Entry entry[KNKMAX]; 
+  Entry entry[KNKMAX];
 
   // leaf information
   // note that Knuth uses indices k..k-1
@@ -206,7 +206,7 @@ class KNLooserTree {
 
   // private member functions
   int initWinner(int root);
-  void updateOnInsert(int node, Key newKey, int newIndex, 
+  void updateOnInsert(int node, Key newKey, int newIndex,
                       Key *winnerKey, int *winnerIndex, int *mask);
   void deallocateSegment(int index);
   void doubleK();
@@ -227,13 +227,13 @@ public:
   void multiMergeUnrolled10(Element *to, int l);
 
   void multiMerge(Element *to, int l); // delete l smallest element to "to"
-  void multiMergeK(Element *to, int l); 
-  int  spaceIsAvailable() { return k < KNKMAX || lastFree >= 0; } 
+  void multiMergeK(Element *to, int l);
+  int  spaceIsAvailable() { return k < KNKMAX || lastFree >= 0; }
      // for new segment
   void insertSegment(Element *to, int sz); // insert segment beginning at to
   int  getSize() { return size; }
   Key getSupremum() { return dummy.key; }
-};  
+};
 
 
 //////////////////////////////////////////////////////////////////////
@@ -257,7 +257,7 @@ class KNHeap {
 
   // how many levels are active
   int activeLevels;
-  
+
   // total size not counting insertBuffer and buffer1
   int size;
 
@@ -282,13 +282,13 @@ public:
 };
 
 
-template <class Key, class Value>  
-inline int KNHeap<Key, Value>::getSize() const 
-{ 
-  return 
-    size + 
-    insertHeap.getSize() + 
-    ((buffer1 + KNBufferSize1) - minBuffer1); 
+template <class Key, class Value>
+inline int KNHeap<Key, Value>::getSize() const
+{
+  return
+    size +
+    insertHeap.getSize() +
+    ((buffer1 + KNBufferSize1) - minBuffer1);
 }
 
 template <class Key, class Value>
